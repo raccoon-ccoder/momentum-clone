d37cb24c014105a6436eb934955ad72231939cf5
raccoon-ccoder <qorwjddus96@naver.com>
2022-01-20 03:00:26 +0900

    220120_로그인 기능 구현 완료 commit date : 220120 comment - 화면 접속시 localStorage의 username 저장 여부에 따른 html element hide - Log In 버튼 클릭시 addEventListener()로 로그인 폼 숨기고 환영 문구 보여주기, localStorage에 사용자명 저장 function() 실행

---
+++ app.js
@@ -0,0 +1,32 @@
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+// Log In 버튼 클릭시 form 태그 사라지고 환영 문구 보여주는 function
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+// 로그인을 한 경우 h1 태그에 사용자명을 보여주고 h1 class hidden을 제거하는 함수
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// 처음 화면 접속시 username이 localStorage에 있는지 확인 후 그에 따른 form or greeting 문구 보여주기
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+const amIFat = null;
+console.log(typeof amIFat === null);    // false
+console.log(amIFat === null);   // true
+
+console.log(test === undefined);
+console.log(typeof test === undefined);
