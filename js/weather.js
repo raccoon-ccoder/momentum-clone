5fff70ed34f00f38e6bfa8a8718c05de56784d38
raccoon-ccoder <qorwjddus96@naver.com>
2022-01-26 07:29:36 +0900

    220126_검색창 html,css 추가 commit date : 220126 comment - 검색창 html, css 추가 - main-top 추가 및 날씨, 검색창창 container css 통합 및 수정

--- js/weather.js
+++ js/weather.js
@@ -3,10 +3,11 @@ const WEATHER_API_KEY = "5105d5697329c5c37681672123a127ed";
 const CITY_API_KEY = "AIzaSyBIbHcjObEteHebcV2mM6n1tr8uFAkDO2k";
 
 function onGeoOk(positon) {
-    console.dir(positon);
     const lat = positon.coords.latitude;
     const lon = positon.coords.longitude;
     const cityUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${CITY_API_KEY}&language=en`;
+    
+    // 위치를 통해 구글 API 이용해 도시명 가져오기
     fetch(cityUrl)
     .then(response => response.json())
     .then(data =>{
@@ -15,6 +16,8 @@ function onGeoOk(positon) {
         city.innerText = cityName;
 
         const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${WEATHER_API_KEY}&units=metric`;
+        
+        // 구글 API를 통한 도시명 이용해서 openweathermap API에서 날씨 정보 가져오기
         fetch(weatherUrl)
         .then(response => response.json())
         .then(data => {
